45648794669cced82558a61f9d23cadf3a94059d
Andy Chen <acchen@cpp.edu>
2022-08-09 13:45:48 -0700

    added types for useRef and event handler, added comments to appBar.css

--- components/appBar/AppBar.tsx
+++ components/appBar/AppBar.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import React, { useEffect, useRef, useState } from 'react'
+import React, { RefObject, useEffect, useRef, useState } from 'react'
 import SearchBox from './SearchBox'
 import NavButton from './NavButton'
 import SignInPopUp from './SignInPopUp'
@@ -11,18 +11,18 @@ const AppBar = () => {
 	const [showSignIn, setShowSignIn] = useState(false);
 
 	// ref to the DOM element inside of SignInPopUp component
-	const signInRef = useRef(null);
+	const signInRef = useRef<HTMLDivElement>(null);
 
 	// toggle sign in box
 	const _setShowSignIn = (): void => {
 		setShowSignIn(!showSignIn);
 	}
 
-	// hoook to close signInPopup if clicked outside the component
-	const _closeSignPopUp = (ref: any) => {
+	// hook to close signInPopup if clicked outside the component
+	const _closeSignPopUp = (ref: RefObject<HTMLDivElement>) => {
 		useEffect(() => {
-			const _handleClickOutside = (event: any) => {
-				if (ref.current && !ref.current.contains(event.target) && event.target.tagName === 'DIV') {
+			const _handleClickOutside = (event: MouseEvent) => {
+				if (ref.current && !ref.current.contains(event.target as Element) && (event.target as Element).tagName === 'DIV') {
 					setShowSignIn(false);
 				}
 			}
@@ -47,8 +47,9 @@ const AppBar = () => {
 
 			<nav className={ appBarStyles.navButtonContainer}>
 				<NavButton scr='/logIn.svg' height={20} width={20} setState={_setShowSignIn}/>
+
 				{/*  semi transparent div background when signin popup appears */}
-				{showSignIn && <div className={ appBarStyles.signInContainerBackground }></div>}
+				{showSignIn && <div className={ `${ appBarStyles.signInContainerBackground } ${ showSignIn ? appBarStyles.openBackground : appBarStyles.closeBackground }` }></div>}
 				<SignInPopUp ref={signInRef} showSignIn={showSignIn}/>
 			</nav>
 		</header>

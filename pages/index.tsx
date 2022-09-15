77bffecea80daac31d5309c89dd39aac4fe3c5d6
AndyCChen <acchen@cpp.edu>
2022-09-14 23:12:44 -0700

    adjusted getMedia query to accept optional season and year arguments

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,11 +6,12 @@ import TrendingBar from '../components/TrendingBar'
 import MediaDisplayBar from '../components/MediaDisplayBar';
 
 type Props = {
-	trendingList: AnimeList;
-	popularList: AnimeList
+	trendingList: AnimeList,
+	popularList: AnimeList,
+	popularCurrentSeasonList: AnimeList,
 }
 
-const Home: NextPage<Props> = ({ trendingList, popularList }) => {
+const Home: NextPage<Props> = ({ trendingList, popularList, popularCurrentSeasonList }) => {
 	return (
 		<>
 			<Head>
@@ -23,21 +24,31 @@ const Home: NextPage<Props> = ({ trendingList, popularList }) => {
 				 }
 			/>
 			<MediaDisplayBar
+				title='POPULAR THIS SEASON'
+				animeList={ popularCurrentSeasonList }
+			/>
+			<MediaDisplayBar
 				title='All TIME POPULAR'
-				animeList={popularList}
+				animeList={ popularList }
 			/>
 		</>
 	)
 }
 
+const getSeason = (month: number) => {
+
+}
+
 export const getServerSideProps: GetServerSideProps = async (context: GetServerSidePropsContext) => {
-	const trendingList = await getMedia({page: 1, perPage: 20, sort: 'TRENDING_DESC'});
-	const popularList = await getMedia({page: 1, perPage: 6, sort: 'POPULARITY_DESC'});
+	const trendingList = await getMedia({ page: 1, perPage: 20, sort: 'TRENDING_DESC' });
+	const popularList = await getMedia({ page: 1, perPage: 6, sort: 'POPULARITY_DESC' });
+	const popularCurrentSeasonList = await getMedia({ page:1, perPage: 6, sort: 'POPULARITY_DESC', season: 'SUMMER'});
 
 	return {
 		props: {
 			trendingList,
 			popularList,
+			popularCurrentSeasonList,
 		}
 	}
 }
--- utils/aniListQueries.tsx
+++ utils/aniListQueries.tsx
@@ -4,33 +4,67 @@ type Props = {
 	page: number,
 	perPage: number,
 	sort: string,
+	season?: string,
+	year?: number,
 };
 
-export const getMedia = async({page, perPage, sort}: Props): Promise<AnimeList> => {
-	let query = `
-		query ($page: Int, $perPage: Int, $sort: [MediaSort]) {
-			Page (page: $page, perPage: $perPage) {
-				media (sort: $sort, type: ANIME) {
-					id,
-					bannerImage,
-					description (asHtml: false),
-					title {
-						romaji,
-						english,
-						native,
-					},
-					coverImage {
-						large,
+/*
+	page: value to determine which page of values to query as results are paginated
+	perPage: value to specify how many items per page
+	sort: how the results are sorted by, for example by trending or popularity
+	season: filter results by seasons, default is undefined, if undefined the query is made without the season argument
+	year: filter results by year, default is set to current year
+*/
+export const getMedia = async({page, perPage, sort, season, year = new Date().getFullYear()}: Props): Promise<AnimeList> => {
+	let query: string;
+
+	if (season !== undefined) {
+		query = 
+			`query ($page: Int, $perPage: Int, $sort: [MediaSort], $season: MediaSeason, $year: Int) {
+				Page (page: $page, perPage: $perPage) {
+					media (sort: $sort, type: ANIME, season: $season, seasonYear: $year) {
+						id,
+						bannerImage,
+						description (asHtml: false),
+						title {
+							romaji,
+							english,
+							native,
+						},
+						coverImage {
+							large,
+						}
 					}
 				}
-			}
-		}`;
+			}`;
+	} else {
+		query = 
+			`query ($page: Int, $perPage: Int, $sort: [MediaSort]) {
+				Page (page: $page, perPage: $perPage) {
+					media (sort: $sort, type: ANIME) {
+						id,
+						bannerImage,
+						description (asHtml: false),
+						title {
+							romaji,
+							english,
+							native,
+						},
+						coverImage {
+							large,
+						}
+					}
+				}
+			}`;
+	}
 
 	let variables = {
 		page: page,
 		perPage: perPage,
 		sort: sort,
-	}
+		season: season,
+		year: year,
+	};
 
 	let url = 'https://graphql.anilist.co';
 	let options = {

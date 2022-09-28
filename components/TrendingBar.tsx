547a55a1c5c14323e73f8668531bb77a8509a37f
AndyCChen <acchen@cpp.edu>
2022-09-27 17:30:29 -0700

    fixed item indicator container height issue with css instead of relying on querying the div height which gave incorrect results

--- components/TrendingBar.tsx
+++ components/TrendingBar.tsx
@@ -3,7 +3,6 @@ import Image from 'next/image';
 import { useCarousel } from '../hooks/useCarousel';
 import { Anime } from '../interfaces/queryInterface';
 import { useMediaQuery } from '../hooks/useMediaQuery';
-import { useElementDimensions } from '../hooks/useElementDimensions';
 
 import TrendingBarStyles from '../styles/TrendingBar.module.css'
 
@@ -16,8 +15,6 @@ const TrendingBar = ({ animeList }: Props) => {
 	const carouselContainerRef = useRef<HTMLDivElement>(null);
 	
 	const {activeIndex, styles} = useCarousel(animeList.length, carouselContainerRef);
-
-	const {elementHeight} = useElementDimensions(carouselContainerRef);
 
 	const height = useMediaQuery(
 		[
@@ -44,24 +41,24 @@ const TrendingBar = ({ animeList }: Props) => {
 
 	return (
 			<div ref={carouselContainerRef} className={ TrendingBarStyles.carouselContainer }>
-				<div className={ TrendingBarStyles.itemIndicatorContainer } style={{height: `${elementHeight}px`}}>
+				<div className={ TrendingBarStyles.itemIndicatorContainer }>
 					{
 						animeList.map((_, index: number) =>
-							<div className={TrendingBarStyles.itemIndicator} style={{backgroundColor: activeIndex === index ? '#418D89' : 'white'}} key={index}></div>
+							<div className={ TrendingBarStyles.itemIndicator } style={{backgroundColor: activeIndex === index ? '#418D89' : 'white'}} key={index}></div>
 						)
 					}
 				</div>
-				<div className={TrendingBarStyles.carouselInner} style={styles}>
-					<div className={ TrendingBarStyles.carouselItem}>
-						<div className={TrendingBarStyles.carouselTextOverlay}>
-							<div className={TrendingBarStyles.textContainer}>
-								<h1>{animeList[animeList.length - 1].title.english}</h1>
-								<p>{parseDescription(animeList[animeList.length - 1].description)}</p>
+				<div className={ TrendingBarStyles.carouselInner } style={styles}>
+					<div className={ TrendingBarStyles.carouselItem }>
+						<div className={ TrendingBarStyles.carouselTextOverlay }>
+							<div className={ TrendingBarStyles.textContainer }>
+								<h1>{ animeList[animeList.length - 1].title.english }</h1>
+								<p>{ parseDescription(animeList[animeList.length - 1].description) }</p>
 							</div>
 						</div>
 						<Image
-							src={animeList[animeList.length - 1].bannerImage}
-							height={height}
+							src={ animeList[animeList.length - 1].bannerImage }
+							height={ height }
 							width={1900}
 							objectFit='cover'
 							layout='responsive'
@@ -71,16 +68,16 @@ const TrendingBar = ({ animeList }: Props) => {
 					{
 						animeList.map((anime: Anime, key: number) =>
 							<div className={ TrendingBarStyles.carouselItem} key={key}>
-								<div className={TrendingBarStyles.carouselTextOverlay}>
-									<div className={TrendingBarStyles.textContainer}>
-										<h1>{anime.title.english}</h1>
-										<p>{parseDescription(anime.description)}</p>
+								<div className={ TrendingBarStyles.carouselTextOverlay }>
+									<div className={ TrendingBarStyles.textContainer }>
+										<h1>{ anime.title.english }</h1>
+										<p>{ parseDescription(anime.description) }</p>
 									</div>
 								</div>
 								<Image
-									src={anime.bannerImage}
-									height={height}
-									width={1900}
+									src={ anime.bannerImage }
+									height={ height }
+									width={ 1900 }
 									objectFit='cover'
 									layout='responsive'
 									draggable='false'
@@ -89,16 +86,16 @@ const TrendingBar = ({ animeList }: Props) => {
 						)
 					}
 					<div className={ TrendingBarStyles.carouselItem}>
-						<div className={TrendingBarStyles.carouselTextOverlay}>
-							<div className={TrendingBarStyles.textContainer}>
-								<h1>{animeList[0].title.english}</h1>
-								<p>{parseDescription(animeList[0].description)}</p>
+						<div className={ TrendingBarStyles.carouselTextOverlay }>
+							<div className={ TrendingBarStyles.textContainer }>
+								<h1>{ animeList[0].title.english }</h1>
+								<p>{ parseDescription(animeList[0].description) }</p>
 							</div>
 						</div>
 						<Image
-							src={animeList[0].bannerImage}
-							height={height}
-							width={1900}
+							src={ animeList[0].bannerImage }
+							height={ height }
+							width={ 1900 }
 							objectFit='cover'
 							layout='responsive'
 							draggable='false'

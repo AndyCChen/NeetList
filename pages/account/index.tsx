fe49f1c53c6fe3ffdaa70d0effc424c4fc561e3d
AndyCChen <acchen@cpp.edu>
2023-11-14 13:28:46 -0800

    finish adding update fields to account page

--- pages/account/index.tsx
+++ pages/account/index.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { GetServerSideProps, GetServerSidePropsContext, NextPage } from 'next'
 import { createPagesServerClient } from "@supabase/auth-helpers-nextjs";
+import { useSupabaseClient } from '@supabase/auth-helpers-react';
 
 import AccountStyles from '../../styles/AccountPage.module.css'
 
@@ -10,33 +11,62 @@ type Props = {
 }
 
 const Account: NextPage<Props> = ({ user_name, user_email }) => {
+	const supabaseClient = useSupabaseClient();
+	
 	const [username, setUsername] = useState(user_name);
+	const previous_username = useRef(user_name);
 	const [updateUsername, setUpdateUsername] = useState(false);
 
 	const onUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		setUsername(e.target.value);
 	}
 
-	const onUsernameUpdate = () => {
+	const onUsernameUpdate = async () => {
+		const { data, error } = await supabaseClient.auth.updateUser({
+			data: {
+				username: username,
+			}
+		});
 
+		if (error) {
+			alert(error.message);
+		}
+		else {
+			alert('Username Updated');
+			previous_username.current = username;
+			setUpdateUsername(false);
+		}
 	}
 
 	useEffect(() => {
-		if (username !== user_name)
+		if (username !== previous_username.current)
 			setUpdateUsername(true);
 		else
 			setUpdateUsername(false);
 	}, [username])
 
 	const [email, setEmail] = useState(user_email);
+	const previous_email = useRef(user_email);
 	const [updateEmail, setUpdateEmail] = useState(false);
 
 	const onEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		setEmail(e.target.value);
 	}
 
-	const onEmailUpdate = () => {
+	const onEmailUpdate = async (e: React.MouseEvent<HTMLFormElement>) => {
+		e.preventDefault();
 
+		const { data, error } = await supabaseClient.auth.updateUser({
+			email: email
+		});
+
+		if (error) {
+			alert(error.message);
+		} else {
+			alert('Email Update Confirmation Sent!');
+			previous_email.current = email;
+			setUpdateEmail(false);
+		}
 	}
 
 	useEffect(() => {
@@ -58,8 +88,26 @@ const Account: NextPage<Props> = ({ user_name, user_email }) => {
 		setPasswordConfirm(e.target.value);
 	}
 
-	const onPasswordUpdate = () => {
-		
+	const onPasswordUpdate = async (e: React.MouseEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		if (password !== passwordConfirm) {
+			alert('Passwords do not match!');
+			return;
+		} 
+
+		const { data, error } = await supabaseClient.auth.updateUser({
+			password: password
+		});
+
+		if (error) {
+			alert(error.message);
+		} else {
+			alert('Password Updated')
+			setPassword('');
+			setPasswordConfirm('');
+			setUpdatePassword(false);
+		}
 	}
 
 	useEffect(() => {
@@ -67,28 +115,43 @@ const Account: NextPage<Props> = ({ user_name, user_email }) => {
 			setUpdatePassword(true);
 		else
 			setUpdatePassword(false);
-	}, [passwordConfirm])
+	}, [passwordConfirm, password])
 
 	return (
 		<div className={ AccountStyles.pageContainer } >
 			<div className={ AccountStyles.pageContent }>
 				<h2>Account</h2>
-				<div className={ AccountStyles.formItem }>
+				<div className={ AccountStyles.formItem } onSubmit={ onUsernameUpdate }>
 					<h1>Username</h1>
 					<input value={ username } onChange={ onUsernameChange }/>
-					{updateUsername && <div>save username</div>}
+					{
+						updateUsername && 
+						<button className={ AccountStyles.saveButton } onClick={ onUsernameUpdate }>
+							Save Username
+						</button>
+					}
 				</div>
-				<div className={ AccountStyles.formItem }>
+				<form className={ AccountStyles.formItem } onSubmit={ onEmailUpdate }>
 					<h1>Email</h1>
 					<input type='email' value={ email } onChange={ onEmailChange }/>
-					{updateEmail && <div>save email</div>}
-				</div>
-				<div className={ AccountStyles.formItem }>
+					{
+						updateEmail && 
+						<button type='submit' className={ AccountStyles.saveButton }>
+							Save Email
+						</button>
+					}
+				</form>
+				<form className={ AccountStyles.formItem } onSubmit={ onPasswordUpdate }>
 					<h1>Change Password</h1>
 					<input type='password' placeholder='New Password' value={ password } onChange={ onPasswordChange }/><br/>
 					<input type='password' placeholder='Confirm Password' value={ passwordConfirm } onChange={ onPasswordConfirmChange }/>
-					{updatePassword && <div>Update Password</div>}
-				</div>
+					{
+						updatePassword && 
+						<button type= 'submit' className={ AccountStyles.saveButton }>
+							Save Password
+						</button>
+					}
+				</form>
 			</div>
 		</div>
 	)
